4b1cbcd264903590532486775da2b415f6d0babe
Ben Trinh <6076614+huantrinh1802@users.noreply.github.com>
2024-04-18 11:23:02 +1000

    [dev]

--- apps/website/src/hooks.server.ts
+++ apps/website/src/hooks.server.ts
@@ -1,10 +1,3 @@
-import { redirect, type HandleServerError } from '@sveltejs/kit';
-
-export const handleError = (() => {
-  // example integration with https://sentry.io/
-  redirect(308, '/');
-}) satisfies HandleServerError;
-
 export async function handle({ event, resolve }) {
   const response = await resolve(event, {
     preload: () => false,
